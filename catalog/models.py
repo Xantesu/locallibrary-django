13548f1713d572e1336120b30e6dad719bce7e95
s72320 <s72320@beuth-hochschule.de>
2019-02-09 14:44:44 +0100

    Almost finished authentication

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -44,7 +46,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
-
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -57,6 +59,14 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (('can_mark_returned', 'Set book as returned'),)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        else:
+            return False
 
     def __str__(self):
         return f'{self.id} ({self.book.title})'
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -21,6 +22,11 @@ def index(request):
     # Books that contain 'buch'
     num_buch = Book.objects.filter(title__contains='buch').count()
 
+    # Number of visits from a user, uses sessions
+    num_visits = request.session.get('num_visits', 0)
+
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -28,9 +34,18 @@ def index(request):
         'num_authors': num_authors,
         'num_fiction': num_fiction,
         'num_buch': num_buch,
+        'num_visits': num_visits
     }
 
     return render(request, 'index.html', context=context)
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+   model = BookInstance
+   template_name = 'catalog/bookinstance_list_borrowed_user.html'
+   paginate_by = 10
+
+   def get_queryset(self):
+       return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
 class BookListView(generic.ListView):
     model = Book
